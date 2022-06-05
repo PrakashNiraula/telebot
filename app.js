4e600a3f3b9afc53a5244c9a24f1e8cc0df28f4f
pinkth3floyd <pinkth3floyd@gmail.com>
2022-06-05 17:01:29 +0545

    auth2

--- app.js
+++ app.js
@@ -11,19 +11,18 @@ var usersRouter = require('./routes/users');
 const categoryrouter=require("./routes/category")
 const subcatrouter=require('./routes/subcategory');
 const productrouter=require('./routes/product');
-<<<<<<< HEAD
+
 require('./telereply')
 var app = express();
 app.use(cors('*'))
 
-=======
 const cors = require('cors');
 
 require('./telereply')
 var app = express();
 
 app.use(cors('*'));
->>>>>>> 4d3c6546bb3077c2209956707e987d9d11d645cc
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -33,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-<<<<<<< HEAD
+
 
 
 app.use('/', indexRouter);
@@ -41,18 +40,7 @@ app.use('/user', usersRouter);
 app.use('/category',auth.verifyUser, categoryrouter);
 app.use('/subcategory',auth.verifyUser, subcatrouter);
 app.use('/product',auth.verifyUser, productrouter);
-=======
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/category', categoryrouter);
-app.use('/subcategory', subcatrouter);
-app.use('/product', productrouter);
-
->>>>>>> 4d3c6546bb3077c2209956707e987d9d11d645cc
-//app.use("/test",)
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
